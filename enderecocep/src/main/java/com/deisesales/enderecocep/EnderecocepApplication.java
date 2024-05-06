aac2be42ee8ff0b6a43771ace2b518762980f2f5
Deise Sales <deisesalles18@gmail.com>
2024-05-06 18:56:04 -0300

    1.0 arquivo

---
+++ enderecocep/src/main/java/com/deisesales/enderecocep/EnderecocepApplication.java
@@ -0,0 +1,13 @@
+package com.deisesales.enderecocep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnderecocepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnderecocepApplication.class, args);
+	}
+
+}
