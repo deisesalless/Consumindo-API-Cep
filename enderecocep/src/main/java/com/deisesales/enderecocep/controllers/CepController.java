ffb753bb9df0af5f8a10dc1acedce99ba8d5e747
Deise Sales <deisesalles18@gmail.com>
2024-05-08 15:38:22 -0300

    2.1 padronizando tipo de resposta http

--- enderecocep/src/main/java/com/deisesales/enderecocep/controllers/CepController.java
+++ enderecocep/src/main/java/com/deisesales/enderecocep/controllers/CepController.java
@@ -4,23 +4,25 @@ import com.deisesales.enderecocep.entities.CepEntity;
 import com.deisesales.enderecocep.services.CepService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/buscarcep")
+@RequestMapping("/consulta")
 public class CepController {
 
     @Autowired
     private CepService service;
 
     @GetMapping("/{cep}")
-    public CepEntity buscar(@PathVariable @Valid String cep) {
-        String url = "http://viacep.com.br/ws/".concat(cep).concat("/json/");
-        var restTemplate = new RestTemplate();
-        var endereco = restTemplate.getForEntity(url, CepEntity.class);
-        return endereco.getBody();
+    public ResponseEntity<CepEntity> buscar(@PathVariable @Valid String cep) {
+        var resposta = service.buscarCep(cep);
+
+        if (resposta == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        return ResponseEntity.status(HttpStatus.FOUND).body(resposta);
     }
 
 }
