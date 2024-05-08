2eaf3461d88b2220e9cdd5777b63f2be42b8bd30
Deise Sales <deisesalles18@gmail.com>
2024-05-08 16:39:03 -0300

    4.0 listar e buscar por id do cliente

--- enderecocep/src/main/java/com/deisesales/enderecocep/controllers/ClienteController.java
+++ enderecocep/src/main/java/com/deisesales/enderecocep/controllers/ClienteController.java
@@ -3,13 +3,13 @@ package com.deisesales.enderecocep.controllers;
 import com.deisesales.enderecocep.dtos.ClienteSalvarDTO;
 import com.deisesales.enderecocep.entities.ClienteEntity;
 import com.deisesales.enderecocep.services.ClienteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cliente")
@@ -19,7 +19,21 @@ public class ClienteController {
     private ClienteService service;
 
     @PostMapping
-    public ResponseEntity<ClienteEntity> salvar(@RequestBody ClienteSalvarDTO data) {
+    public ResponseEntity<ClienteEntity> salvar(@RequestBody @Valid ClienteSalvarDTO data) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.salvar(data));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteEntity>> listarTodos() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ClienteEntity> buscarPorID(@PathVariable Integer id) {
+        var cliente = service.buscarPorID(id);
+
+        return cliente.map(clienteEntity ->
+                ResponseEntity.status(HttpStatus.FOUND).body(clienteEntity)).orElseGet(() ->
+                ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 }
--- enderecocep/src/main/java/com/deisesales/enderecocep/services/ClienteService.java
+++ enderecocep/src/main/java/com/deisesales/enderecocep/services/ClienteService.java
@@ -6,6 +6,9 @@ import com.deisesales.enderecocep.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ClienteService {
 
@@ -15,5 +18,13 @@ public class ClienteService {
     public ClienteEntity salvar(ClienteSalvarDTO data) {
         var cliente = new ClienteEntity(data.nomeCompleto(), data.cpf(), data.email());
         return repository.save(cliente);
+    }
+
+    public List<ClienteEntity> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<ClienteEntity> buscarPorID(Integer id) {
+        return repository.findById(id);
     }
 }
